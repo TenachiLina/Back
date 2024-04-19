0b98088937621e14794e66978c97357327c74cb5
TenachiLina <linatenachi@gmail.com>
2024-04-19 08:48:14 +0100

    BackEnd Start

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const db = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+    password: '',
+    database: 'pharm',
+
+})
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+
+app.get("/",(req, res) =>{
+   const sqlInsert = "INSERT INTO produits(NomProd, PrixUnitaire, TauxTVA, Stock, `Quantité d'alerte`) VALUES ('Paracétamol',25,0.75,900,10);"
+    db.query(sqlInsert,(err,result)=>{
+        res.send("Hi I'm Lily548")
+
+    });
+});
+
+app.listen(3002,() =>{
+    console.log("running on port 3002");
+});
